f300db19e654222b279b64a51f23793c261f9e7f
wescules <wafermonster@yahoo.com>
2019-04-19 14:11:39 -0500

    Libary and read from text file
    Libary and read from text file

---
+++ library final project/library final project/Library.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+#ifndef LIBRARY_H 
+#define LIBRARY_H
+
+struct book {
+	int id;
+	string title, authors, status;
+	string publisher, publicationYear, ISBN;
+};
+
+class Library {
+private:
+	vector<book> books;
+public:
+	Library() {
+		ifstream infile("books.txt");
+		string line;
+		int i = 0;
+		while (getline(infile, line)) {
+			books.push_back(book());
+			books[i].id = stoi(line.substr(line.find(":") + 1)); getline(infile, line);
+			books[i].title = line.substr(line.find(":") + 1); getline(infile, line);
+			books[i].authors = line.substr(line.find(":") + 1); getline(infile, line);
+			books[i].ISBN = line.substr(line.find(":") + 1); getline(infile, line);
+			books[i].publisher = line.substr(line.find(":") + 1); getline(infile, line);
+			books[i].publicationYear = line.substr(line.find(":") + 1); getline(infile, line);
+			books[i].status = line.substr(line.find(":") + 2); getline(infile, line);
+			i++;
+		}
+	}
+
+	void printAllAvalibleBooks() {
+		for (auto i : books) {
+			cout << i.title << endl;
+		}
+	}
+	
+};
+
+#endif
---
+++ library final project/library final project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Library.h"
+using namespace std;
+
+int main() {
+	Library l;
+	l.printAllAvalibleBooks();
+
+	system("pause");
+	return 0;
+}
